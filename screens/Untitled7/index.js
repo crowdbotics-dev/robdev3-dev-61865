95ec024f43d0d29c8ce46c833aa7d2982d390106
Robert So <robkso@gmail.com>
2023-05-08 12:02:11 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled7/index.js
+++ screens/Untitled7/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { Text } from "react-native";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled7 = () => {
@@ -8,13 +8,20 @@ const Untitled7 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><Text style={styles.vaQrKOeB}>{"Working"}</Text></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  vaQrKOeB: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
   }
 });
 export default Untitled7;
