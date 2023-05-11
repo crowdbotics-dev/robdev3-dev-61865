6ea65f70b7d78d09a9725a6dba5e9ec9f336db79
Robert So <robkso@gmail.com>
2023-05-10 22:32:33 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled4/index.js
+++ screens/Untitled4/index.js
@@ -4,20 +4,22 @@ import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled4 = () => {
   return <SafeAreaView style={styles.safeArea}>
-        <ScrollView contentContainerStyle={{
-      backgroundColor: '#f0f0f1',
+      <ScrollView contentContainerStyle={{
+      backgroundColor: "#f0f0f1",
       padding: 10,
-      position: 'relative',
+      position: "relative",
       flex: 1
-    }}><ImageBackground style={styles.qqJezCab} source={{
+    }}>
+        <ImageBackground style={styles.qqJezCab} source={{
         uri: "https://s3.amazonaws.com/gs-geo-images/d766f76f-8bdc-4fc2-80ed-b3ac5f00385b.png"
-      }} resizeMode="cover"></ImageBackground></ScrollView>
+      }} resizeMode="cover"></ImageBackground>
+      </ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
-    height: '100%'
+    height: "100%"
   },
   qqJezCab: {
     width: 338,
--- screens/Untitled8/index.js
+++ screens/Untitled8/index.js
@@ -1,20 +1,23 @@
+import { Text } from "react-native";
 import React from "react";
 import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled8 = () => {
   return <SafeAreaView style={styles.safeArea}>
-        <ScrollView contentContainerStyle={{
-      backgroundColor: '#f0f0f1',
+      <ScrollView contentContainerStyle={{
+      backgroundColor: "#f0f0f1",
       padding: 10,
-      position: 'relative',
+      position: "relative",
       flex: 1
-    }}><View style={styles.jlLSRYqX}></View></ScrollView>
+    }}>
+        <View style={styles.jlLSRYqX}></View>
+      <Text style={styles.eVMGPzTY}>Lorem ipsum…</Text></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
-    height: '100%'
+    height: "100%"
   },
   jlLSRYqX: {
     height: 60,
@@ -22,6 +25,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#E4E4E4",
     borderRadius: 0,
     color: "#777777"
+  },
+  eVMGPzTY: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
   }
 });
 export default Untitled8;
